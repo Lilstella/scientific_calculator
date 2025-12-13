3b04be0e242ac7a1a55ad59b897b86b29eb55ae3
Lilstella <luisstella.m@gmail.com>
2025-12-13 16:29:43 -0300

    Add benchmarking for taylor series

---
+++ benchmark/bench_taylor.jl
@@ -0,0 +1,65 @@
+using BenchmarkTools
+using scientific_calculator
+
+calc_fast = Calculator(fast_mode=true)
+calc_slow = Calculator(fast_mode=false)
+
+calc_fast_m = Calculator(fast_mode=true, terms=20)
+calc_slow_m = Calculator(fast_mode=false, terms=20)
+
+calc_fast_l = Calculator(fast_mode=true, terms=50)
+calc_slow_l = Calculator(fast_mode=false, terms=50)
+
+x = π / 4
+
+println("Benchmarking Factorial Function:")
+println("\n10!:")
+@btime factorial!($calc_fast, 10)
+@btime factorial!($calc_slow, 10)
+
+println("\n20!:")
+@btime factorial!($calc_fast, 20)
+@btime factorial!($calc_slow, 20)
+
+println("\n50!:")
+@btime factorial!($calc_fast, 150)
+@btime factorial!($calc_slow, 150)
+
+println("Benchmarking Sine Function:")
+println("\nx = $x")
+@btime sin!($calc_fast, $x)
+@btime sin!($calc_slow, $x)
+
+println("\nx = $x, terms=20")
+@btime sin!($calc_fast_m, $x)
+@btime sin!($calc_slow_m, $x)
+
+println("\nx = $x, terms=50")
+@btime sin!($calc_fast_l, $x)
+@btime sin!($calc_slow_l, $x)
+
+println("Benchmarking Cosine Function:")
+println("\nx = $x")
+@btime cos!($calc_fast, $x)
+@btime cos!($calc_slow, $x)
+
+println("\nx = $x, terms=20")
+@btime cos!($calc_fast_m, $x)
+@btime cos!($calc_slow_m, $x)
+
+println("\nx = $x, terms=50")
+@btime cos!($calc_fast_l, $x)
+@btime cos!($calc_slow_l, $x)
+
+println("Benchmarking Exponential Function:")
+println("\nx = $x")
+@btime exp!($calc_fast, $x)
+@btime exp!($calc_slow, $x)
+
+println("\nx = $x, terms=20")
+@btime exp!($calc_fast_m, $x)
+@btime exp!($calc_slow_m, $x)
+
+println("\nx = $x, terms=50")
+@btime exp!($calc_fast_l, $x)
+@btime exp!($calc_slow_l, $x)
--- src/scientific_calculator.jl
+++ src/scientific_calculator.jl
@@ -8,12 +8,13 @@ export Calculator,
 include("taylor/taylor.jl")
 using .taylor
 
-mutable struct Calculator
-    value::Real
+mutable struct Calculator{T<:Real}
+    value::T
     fast_mode::Bool
+    terms::Int
 end
 
-Calculator(; fast_mode=false) = Calculator(0.0, fast_mode)
+Calculator(; fast_mode=false, terms=10) = Calculator(0.0, fast_mode, terms)
 
 add!(calc::Calculator, a::Float64, b::Float64) = calc.value = a + b
 
@@ -25,14 +26,16 @@ divide!(calc::Calculator, a::Float64, b::Float64) = b == 0.0 ? throw(DivideError
 
 factorial!(calc::Calculator, n::Int) = calc.value = calc.fast_mode ? f_factorial(n) : r_factorial(n)
 
-sin!(calc::Calculator, x::Real) = calc.value = calc.fast_mode ? f_sin(x) : r_sin(x)
+sin!(calc::Calculator, x::Real) = calc.value = calc.fast_mode ? f_sin(x, calc.terms) : r_sin(x, 0, calc.terms)
 
-cos!(calc::Calculator, x::Real) = calc.value = calc.fast_mode ? f_cos(x) : r_cos(x)
+cos!(calc::Calculator, x::Real) = calc.value = calc.fast_mode ? f_cos(x, calc.terms) : r_cos(x, 0, calc.terms)
 
-exp!(calc::Calculator, x::Real) = calc.value = calc.fast_mode ? f_exp(x) : r_exp(x)
+exp!(calc::Calculator, x::Real) = calc.value = calc.fast_mode ? f_exp(x, calc.terms) : r_exp(x, 0, calc.terms)
 
 restart!(calc::Calculator) = calc.value = 0.0
 
 velocity!(calc::Calculator, fast_mode::Bool) = calc.fast_mode = fast_mode
+
+precision!(calc::Calculator, terms::Int) = calc.terms = terms
 
 end
